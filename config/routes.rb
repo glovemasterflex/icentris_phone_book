f2f5aeee291f5afcf9db5cfc83015044b4660247
Daniel Glover <gloversdaniel@gmail.com>
2016-07-28 19:36:09 -0600

    added contact model and controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+
+  get 'contacts/show'
+
+  get 'contacts/new'
+
+  get 'contacts/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
