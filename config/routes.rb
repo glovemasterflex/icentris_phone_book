2e6444009faea5cf7be214ddc5e9c0c069142df5
Daniel Glover <gloversdaniel@gmail.com>
2016-07-28 19:54:53 -0600

    added functionality to controller for index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-
-  get 'contacts/show'
-
-  get 'contacts/new'
-
-  get 'contacts/edit'
+  root "contacts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
